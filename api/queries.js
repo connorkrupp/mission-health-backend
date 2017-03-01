177566d0916a65c799b36f8d1d4db5e1c2926ea3
Connor Krupp <kruppcon@umich.edu>
2017-03-01 11:45:44 -0500

    Added heroku config

--- api/queries.js
+++ api/queries.js
@@ -6,7 +6,7 @@ var options = {
 };
 
 var pgp = require('pg-promise')(options);
-var connectionString = 'postgres://localhost:5432/healthfit'; // startrek is an example database name
+var connectionString = process.env.DATABASE_URL;
 var db = pgp(connectionString);
 
 
